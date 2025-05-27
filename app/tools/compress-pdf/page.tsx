5cb92a07f2d72857f80c4f44d42b6319b761d3c9
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-27 20:51:34 +0700

    edit text color

--- app/tools/compress-pdf/page.tsx
+++ app/tools/compress-pdf/page.tsx
@@ -10,7 +10,7 @@ export default function ToolPage() {
     <div className="bg-zinc-950 min-h-screen text-white">
       <Navbar />
       <main className="p-4 max-w-3xl mx-auto min-h-[70vh]">
-        <h1 className="text-3xl font-bold mb-4 text-white">Compress PDF</h1>
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">Compress PDF</h1>
         <p className="text-gray-400 mb-6">Kompres ukuran file PDF tanpa mengurangi kualitas.</p>
 
         <div className="bg-white p-6 rounded-lg shadow">

8264bee1ed8378c5907055a43d979eb8ed403235
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-05-27 20:54:36 +0700

    edit text color

--- app/tools/text-to-speech/page.tsx
+++ app/tools/text-to-speech/page.tsx
@@ -9,7 +9,7 @@ export default function ToolPage() {
     <div className="bg-zinc-950 min-h-screen text-white">
       <Navbar />
       <main className="p-4 max-w-3xl mx-auto min-h-[70vh]">
-        <h1 className="text-3xl font-bold mb-4 text-white">Text to Speech</h1>
+        <h1 className="text-3xl font-bold mb-4 text-gray-800">Text to Speech</h1>
         <p className="text-gray-400 mb-6">Ubah teks menjadi suara.</p>
 
         <div className="bg-white p-6 rounded-lg shadow">
